8854c45e4417872b422435c8376139c5cf97e2a0
alex <alex.embrey@apexrms.com>
2021-02-27 09:34:11 -0800

    Update model for new NULL iterations feature

--- src/data-bc-cdc.R
+++ src/data-bc-cdc.R
@@ -1,46 +1,44 @@
 # data-bc-cdc.R
 # Downloads data from the BC CDC website for use in the SyncroSim epi Package
 
-# Input Datasheets:
+# Datasheets:
 #   dataBcCdc_Input - optional user-specified download URL
-
-# Output Datasheets:
-#   dataBcCdc_Input - actual download URL
-#   dataBcCdc_Output - copy of downloaded CSV filename and raw data
+#                   - copy of downloaded CSV filename and raw data
+#                   - date and time of the download
 #   epi_DataSummary - case data in summary format
-#   epi_RunControl - start and end dates of raw downloaded data
+#   epi_RunControl  - start and end dates of raw downloaded data
 
 library(rsyncrosim)
 library(tidyverse)
 library(lubridate)
 library(data.table)
 
-# Get the scenario that is currently being run
+# Get the environment 
 env <- ssimEnvironment()
+transferDir = env$TransferDirectory
+
+# Get the scenario that is currently being run
 myScenario <- scenario()
 
-# Retrieve the download URL (use default if not specified)
+# Get the required datasheets
 inputSheet <- datasheet(myScenario, "dataBcCdc_Input")
+inputSheet <- transform(inputSheet, DownloadDateTime = as.character(DownloadDateTime))
+summarySheet <- datasheet(myScenario, "epi_DataSummary", empty = T)
+runControl <- datasheet(myScenario, "epi_RunControl", empty = T)
+
+# Download the raw data and process
 downloadUrl <- inputSheet$RegionalSummaryDataURL
-if (length(downloadUrl) == 0) downloadUrl <- "http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Regional_Summary_Data.csv"
-
-# Download the raw data
+if (length(downloadUrl) == 0) stop("URL Missing.")
 regionalSummaryData <- read.csv(downloadUrl)
+csvFileName <- paste(transferDir, "RegionalSummaryData.csv", sep = "/")
+write.csv(regionalSummaryData, csvFileName)
 
-# Save Url actually used
+# Save input parameters used
 inputSheet[1,] <-NA
 inputSheet$RegionalSummaryDataURL <- downloadUrl
+inputSheet$RegionalSummaryDataFile <- csvFileName
+inputSheet$DownloadDateTime = as.character(Sys.time())
 saveDatasheet(myScenario, inputSheet, "dataBcCdc_Input")
-
-# Save a copy of the raw data to CSV
-csvFileName <- paste(env$TransferDirectory, "RegionalSummaryData.csv", sep = "/")
-write.csv(regionalSummaryData, csvFileName)
-outputSheet <- datasheet(myScenario, "dataBcCdc_Output", empty = T)
-outputSheet <- transform(outputSheet, DownloadDateTime = as.character(DownloadDateTime))
-
-# Save the filename of the raw data CSV
-outputSheet <- addRow(outputSheet, c(csvFileName, as.character(Sys.time())))
-saveDatasheet(myScenario, outputSheet, "dataBcCdc_Output")
 
 # Import the data into the DataSummary datasheet
 
@@ -55,10 +53,7 @@ cases <- data.table(regionalSummaryData) %>%
   rename(value=Cases_Reported) %>%
   dplyr::as_tibble()
 
-summarySheet <- datasheet(myScenario, "epi_DataSummary", empty = T)
 summarySheet[nrow(cases),] <- NA
-
-summarySheet$Iteration <- 1
 summarySheet$Timestep <- cases$date
 summarySheet$Variable <- "Cases"
 summarySheet$Jurisdiction <- "Canada - British Columbia"
@@ -70,11 +65,10 @@ summarySheet$Value <- cases$value
 saveDatasheet(myScenario, summarySheet, "epi_DataSummary")
 
 # Save the start and end date of the raw data to Run Control datasheet
-runControl <- datasheet(myScenario, "epi_RunControl", empty = T)
+
 runControl[1,] <- NA
 runControl$MinimumTimestep <- min(summarySheet$Timestep)
 runControl$MaximumTimestep <- max(summarySheet$Timestep)
-runControl$MinimumIteration <- 1
-runControl$MaximumIteration <- 1
 
 saveDatasheet(myScenario, runControl, name = "epi_RunControl")
+
