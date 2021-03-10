54d5121269c586f23184a0eea0f88d791b28202f
colindaniel <colin.daniel@apexrms.com>
2021-03-10 15:45:38 -0500

    Updated with new dataframe layout. Automatically adds jurisdiction and variables

--- src/data-bc-cdc.R
+++ src/data-bc-cdc.R
@@ -1,6 +1,9 @@
 # data-bc-cdc.R
 # Downloads data from the BC CDC website for use in the SyncroSim epi Package
 
+# TODO:
+# -Use Run Control to filter the download dates (if provided)
+
 # Datasheets:
 #   dataBcCdc_Input - optional user-specified download URL
 #                   - copy of downloaded CSV filename and raw data
@@ -13,6 +16,11 @@ library(tidyverse)
 library(lubridate)
 library(data.table)
 
+# Set the epi package project-scoped strings for jurisdiction and variables
+jurisdictionBC <- "Canada - British Columbia"
+casesDailyVar <- "Cases - Daily"
+casesCumVar <- "Cases - Cumulative"
+
 # Get the environment 
 env <- ssimEnvironment()
 transferDir = env$TransferDirectory
@@ -20,25 +28,31 @@ transferDir = env$TransferDirectory
 # Get the scenario that is currently being run
 myScenario <- scenario()
 
-# Get the required datasheets
-inputSheet <- datasheet(myScenario, "dataBcCdc_Input")
-inputSheet <- transform(inputSheet, DownloadDateTime = as.character(DownloadDateTime))
+# Add the required variables and jurisdictions to the SyncroSim project (will ignore if they exist already)
+saveDatasheet(myScenario, data.frame(Name = jurisdictionBC), "epi_Jurisdiction")
+saveDatasheet(myScenario, data.frame(Name = c(casesDailyVar, casesCumVar)), "epi_Variable")
+
+# Get the required scenario-scoped datasheets
+inputSheet <- datasheet(myScenario, "dataBcCdc_Inputs")
+outputSheet <- datasheet(myScenario, "dataBcCdc_Outputs")
+outputSheet <- transform(outputSheet, DownloadDateTime = as.character(DownloadDateTime))
 summarySheet <- datasheet(myScenario, "epi_DataSummary", empty = T)
 runControl <- datasheet(myScenario, "epi_RunControl", empty = T)
+runtimeJurisdiction <- datasheet(myScenario, "epi_RuntimeJurisdiction", empty = T)
 
 # Download the raw data and process
 downloadUrl <- inputSheet$RegionalSummaryDataURL
 if (length(downloadUrl) == 0) stop("URL Missing.")
 regionalSummaryData <- read.csv(downloadUrl)
-csvFileName <- paste(transferDir, "RegionalSummaryData.csv", sep = "/")
+csvFileName <- paste(transferDir, "Regional_Summary_Data.csv", sep = "/")
 write.csv(regionalSummaryData, csvFileName)
 
 # Save input parameters used
-inputSheet[1,] <-NA
-inputSheet$RegionalSummaryDataURL <- downloadUrl
-inputSheet$RegionalSummaryDataFile <- csvFileName
-inputSheet$DownloadDateTime = as.character(Sys.time())
-saveDatasheet(myScenario, inputSheet, "dataBcCdc_Input")
+outputSheet[1,] <-NA
+# inputSheet$RegionalSummaryDataURL <- downloadUrl
+outputSheet$RegionalSummaryDataFile <- csvFileName
+outputSheet$DownloadDateTime = as.character(Sys.time())
+saveDatasheet(myScenario, outputSheet, "dataBcCdc_Outputs")
 
 # Import the data into the DataSummary datasheet
 
@@ -55,12 +69,17 @@ cases <- data.table(regionalSummaryData) %>%
 
 summarySheet[nrow(cases),] <- NA
 summarySheet$Timestep <- cases$date
-summarySheet$Variable <- "Cases - Daily"
-summarySheet$Jurisdiction <- "Canada - British Columbia"
+summarySheet$Variable <- casesDailyVar
+summarySheet$Jurisdiction <- jurisdictionBC
 summarySheet$AgeMin <- NULL
 summarySheet$AgeMax <- NULL
 summarySheet$Sex <- NULL
 summarySheet$Value <- cases$value
+
+# Add cumulative cases
+summarySheet <- mutate(summarySheet, Value = cumsum(Value)) %>%
+  mutate(Variable = casesCumVar) %>%
+  bind_rows(summarySheet)
 
 saveDatasheet(myScenario, summarySheet, "epi_DataSummary")
 
@@ -71,4 +90,10 @@ runControl$MinimumTimestep <- min(summarySheet$Timestep)
 runControl$MaximumTimestep <- max(summarySheet$Timestep)
 
 saveDatasheet(myScenario, runControl, name = "epi_RunControl")
+
+# Save the jurisdiction of the raw data to the Runtime Jurisdiction datasheet
+runtimeJurisdiction[1,] <- NA
+runtimeJurisdiction$Jurisdiction <- jurisdictionBC
+saveDatasheet(myScenario, runtimeJurisdiction, name = "epi_RuntimeJurisdiction")
+
 
