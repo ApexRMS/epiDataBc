cbb153fb344a56ab3abc29c409e3f3277851bf90
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2021-04-17 15:00:59 -0700

    Allow disaggretation by health region

--- src/data-bc-cdc.R
+++ src/data-bc-cdc.R
@@ -20,21 +20,20 @@ transferDir = env$TransferDirectory
 myScenario <- scenario()
 
 # Add the required jurisdictions and variables to the SyncroSim project (will ignore if they exist already)
-saveDatasheet(myScenario, data.frame(Name = jurisdictionBC), "epi_Jurisdiction")
 saveDatasheet(myScenario, data.frame(Name = c(casesDailyVar, casesCumVar)), "epi_Variable")
 
 # Get the required scenario-scoped input datasheet
 inputSheet <- datasheet(myScenario, "epiDataBc_Settings")
 
 # Download the raw data and process
-downloadUrl <- inputSheet$RegionalSummaryDataURL
+downloadUrl <- "http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Regional_Summary_Data.csv"
 if (length(downloadUrl) == 0) stop("URL Missing.")
 regionalSummaryData <- read.csv(downloadUrl)
-csvFileName <- paste(transferDir, "Regional_Summary_Data.csv", sep = "/")
+csvFileName <- paste(transferDir, "BC_CDC_Data.csv", sep = "/")
 write.csv(regionalSummaryData, csvFileName)
 
 outputSheet = data.frame(
-  RegionalSummaryDataFile = csvFileName,
+  DataFile = csvFileName,
   DownloadDateTime = as.character(Sys.time()),
   MinimumTimestep = as.character(min(as.character(regionalSummaryData$Date))),
   MaximumTimestep = as.character(max(as.character(regionalSummaryData$Date)))
@@ -43,31 +42,48 @@ saveDatasheet(myScenario, outputSheet, "epiDataBc_ResultsRaw")
 
 # Import the data into the DataSummary datasheet
 
+# Choose which health regions to keep
+if(inputSheet$Regions) {
+  regionsToKeep <- regionalSummaryData$HA %>%
+    unique() %>%
+    str_subset("All", negate = T) %>%
+    str_subset("Out of Canada", negate = T)
+  saveDatasheet(myScenario, data.frame(Name = str_c(jurisdictionBC, " - ", regionsToKeep)), "epi_Jurisdiction")
+} else {
+  regionsToKeep <- "All"
+  saveDatasheet(myScenario, data.frame(Name = jurisdictionBC), "epi_Jurisdiction")
+}
+
 # Filtering the downloaded case data. We'll take the combined data from all health authorities, with all
 # cases occurring after Day0, and renaming the Date and Case_Reported columns as "date" and "value"
 # respectively.
 cases <- data.table(regionalSummaryData) %>%
   separate(col=Date, into=c("date"), sep=' ') %>%
   mutate_at(vars(date), as.IDate) %>%
-  subset(HA=="All" & Province=="BC") %>%
-  select(date, Cases_Reported) %>%
-  rename(value=Cases_Reported) %>%
+  subset(HA %in% regionsToKeep & Province=="BC") %>%
+  select(date, value = Cases_Reported, Jurisdiction = HA, Subregions = HSDA) %>%
+  mutate(Jurisdiction = str_c(jurisdictionBC, " - ", Jurisdiction)) %>%
   dplyr::as_tibble()
+
+if(inputSheet$Regions)
+  cases <- cases %>% filter(Subregions != "All")
 
 summarySheet <- datasheet(myScenario, "epi_DataSummary", empty = T, optional=T)
 summarySheet[nrow(cases),] <- NA
 summarySheet$TransformerID <- transformerName
 summarySheet$Timestep <- cases$date
 summarySheet$Variable <- casesDailyVar
-summarySheet$Jurisdiction <- jurisdictionBC
-# summarySheet$AgeMin <- NA
-# summarySheet$AgeMax <- NULL
-# summarySheet$Sex <- NULL
+if(!inputSheet$Regions) {
+  summarySheet$Jurisdiction <- jurisdictionBC
+} else
+  summarySheet$Jurisdiction <- cases$Jurisdiction
 summarySheet$Value <- cases$value
 
 # Add cumulative cases
 summarySheet <- mutate(summarySheet, Value = cumsum(Value)) %>%
+  group_by(Jurisdiction) %>%
   mutate(Variable = casesCumVar) %>%
+  ungroup() %>%
   bind_rows(summarySheet)
 
 saveDatasheet(myScenario, summarySheet, "epi_DataSummary")
